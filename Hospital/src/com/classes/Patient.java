1735ebbd6685ae4a0dca6bb4cc576fcc3c674292
Ramit Gupta <gupta.ram@northeastern.edu>
2022-10-29 17:18:10 -0400

    Create - Patient getter and setter

---
+++ Hospital/src/com/classes/Patient.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.classes;
+
+/**
+ *
+ * @author dotaw
+ */
+public class Patient extends Person{
+    private String uniqueID;
+
+    public String getUniqueID() {
+        return uniqueID;
+    }
+
+    public void setUniqueID(String uniqueID) {
+        this.uniqueID = uniqueID;
+    }
+}
