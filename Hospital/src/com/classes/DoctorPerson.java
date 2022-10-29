8e91e8d11588b31e561134e291fccf1fa6d200c6
Ramit Gupta <gupta.ram@northeastern.edu>
2022-10-29 17:41:12 -0400

    Create - Doctor getter setter

---
+++ Hospital/src/com/classes/DoctorPerson.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.classes;
+
+/**
+ *
+ * @author dotaw
+ */
+public class DoctorPerson extends Person{
+    private String mdeicalLicenseNumber;
+
+    public String getMdeicalLicenseNumber() {
+        return mdeicalLicenseNumber;
+    }
+
+    public void setMdeicalLicenseNumber(String mdeicalLicenseNumber) {
+        this.mdeicalLicenseNumber = mdeicalLicenseNumber;
+    }
+}
