08ee6a1eaec798a5a3f5afe930cb083a7257029f
Ramit Gupta <gupta.ram@northeastern.edu>
2022-10-29 17:20:45 -0400

    Create - Patient directory Array List

---
+++ Hospital/src/com/classes/PatientDirectory.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.classes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dotaw
+ */
+public class PatientDirectory {
+   
+    private ArrayList<Patient> dir = new ArrayList<>();
+    
+    public PatientDirectory(){
+        this.dir = new ArrayList<>();
+    }
+
+    public void setDir(ArrayList<Patient> dir) {
+        this.dir = dir;
+    }
+    
+    public ArrayList<Patient> getDir() {
+        return dir;
+    }
+    
+    public Patient addPerson(){
+        Patient person = new Patient();
+        dir.add(person);
+        return person;
+    }
+}
+
