fa9deba75c7c2de5a002ae4019ac383403b563cf
Ramit Gupta <gupta.ram@northeastern.edu>
2022-10-29 17:17:37 -0400

    Create - Person getter and setter

---
+++ Hospital/src/com/classes/Person.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.classes;
+
+import java.util.Date;
+
+/**
+ *
+ * @author dotaw
+ */
+public class Person {
+    public String username;
+    public String password;
+    public String role;
+    public String mail;
+    
+    public String getAccountStatus() {
+        return AccountStatus;
+    }
+
+    public void setAccountStatus(String AccountStatus) {
+        this.AccountStatus = AccountStatus;
+    }
+    private int phone;
+    private String gender;
+    private Date dateOfBirth;
+    private String AccountStatus = "Active";
+
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
