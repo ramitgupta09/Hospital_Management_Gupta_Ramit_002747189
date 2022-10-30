95b94a3be8a9cd35c3c26e2c23cf7f47c57bca08
Ramit Gupta <gupta.ram@northeastern.edu>
2022-10-30 12:50:26 -0400

    Create - array lists

---
+++ Hospital/src/com/classes/System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.classes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dotaw
+ */
+public class System {
+    private ArrayList<Person> patientDir = new ArrayList<>();
+    private ArrayList<DoctorPerson> doctorDir = new ArrayList<>();
+}
