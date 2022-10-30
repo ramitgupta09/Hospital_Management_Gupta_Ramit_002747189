774c524d3a7a36d981a68bfdc3d8b31e5ad14a08
Ramit Gupta <gupta.ram@northeastern.edu>
2022-10-29 23:30:07 -0400

    Create - Vital Signs getter and setter

---
+++ Hospital/src/com/classes/VitalSigns.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.classes;
+
+/**
+ *
+ * @author dotaw
+ */
+public class VitalSigns {
+    private int heartRate;
+    private int height;
+    private int bloodPressure;
+    private int bodyTemperatur;
+    private int respirationRate;
+
+    public int getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(int heartRate) {
+        this.heartRate = heartRate;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(int bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public int getBodyTemperatur() {
+        return bodyTemperatur;
+    }
+
+    public void setBodyTemperatur(int bodyTemperatur) {
+        this.bodyTemperatur = bodyTemperatur;
+    }
+
+    public int getRespirationRate() {
+        return respirationRate;
+    }
+
+    public void setRespirationRate(int respirationRate) {
+        this.respirationRate = respirationRate;
+    }
+}
