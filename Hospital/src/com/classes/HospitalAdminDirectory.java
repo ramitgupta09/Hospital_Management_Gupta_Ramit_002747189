d5e35fd9b07b0bcdec25d5192cd7945ebe6580b4
Ramit Gupta <gupta.ram@northeastern.edu>
2022-10-31 03:21:36 -0400

    Create - Hospital Admin Directory Array List and addHospitalAdmin()

---
+++ Hospital/src/com/classes/HospitalAdminDirectory.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.classes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dotaw
+ */
+public class HospitalAdminDirectory {
+    private ArrayList<HospitalAdmin> hosAdmin = new ArrayList<>();
+    
+    public HospitalAdminDirectory(){
+    this.hosAdmin = new ArrayList<>();
+    }
+
+    public ArrayList<HospitalAdmin> getHosAdmin() {
+        return hosAdmin;
+    }
+
+    public void setHosAdmin(ArrayList<HospitalAdmin> hosAdmin) {
+        this.hosAdmin = hosAdmin;
+    }
+    
+    public HospitalAdmin addHospitalAdmin(){
+    HospitalAdmin hoAd = new HospitalAdmin();
+    hosAdmin.add(hoAd);
+    return hoAd;
+    }
+}
