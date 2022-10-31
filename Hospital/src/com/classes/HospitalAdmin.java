85a8987caf08100d1128af82af9e66d30035e209
Ramit Gupta <gupta.ram@northeastern.edu>
2022-10-31 03:16:03 -0400

    Create - Hospital Admin getter and setter

---
+++ Hospital/src/com/classes/HospitalAdmin.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.classes;
+
+/**
+ *
+ * @author dotaw
+ */
+public class HospitalAdmin extends Person{
+    private String hospitalAdminID;
+
+    public String getHospitalAdminID() {
+        return hospitalAdminID;
+    }
+
+    public void setHospitalAdminID(String hospitalAdminID) {
+        this.hospitalAdminID = hospitalAdminID;
+    }
+}
